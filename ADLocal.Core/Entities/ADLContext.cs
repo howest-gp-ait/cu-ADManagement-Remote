e309cbb6976aa3adfa1292e8f846217aab4c236b
Jan De Deurwaerder <55309715+jandedeurwaerder-howest@users.noreply.github.com>
2023-04-18 08:25:20 +0200

    Update ADLContext.cs

--- ADLocal.Core/Entities/ADLContext.cs
+++ ADLocal.Core/Entities/ADLContext.cs
@@ -4,7 +4,7 @@
     {
         public static string contextLDAPPrefix = "hier komt je persoonlijke info";
         public static string contextLDAPRoot = "hier komt je persoonlijke info";
-        public static string contextAddress = "172.21.24.21";
+        public static string contextAddress = "172.21.25.21";
         public static string contextRootDomain = "hier komt je persoonlijke info";
         public static string contextUsername = "hier komt je persoonlijke info";
         public static string contextPw = "hier komt je persoonlijke info";
